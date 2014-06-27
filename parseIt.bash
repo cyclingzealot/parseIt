b25215506aad8b01c81f6a469f2f22a271314c02
Julien Lamarche <jlam@credil.org>
2014-06-27 16:35:14 -0400

    now outputs JSON validator if script things its invalid JSON

--- parseIt.bash
+++ parseIt.bash
@@ -3,11 +3,33 @@
 while(true); do
     read file
     validateJSON.bash $file > /dev/null 2>&1
-    result=$?
+    validJSON=$?
+
+    firstNonWhiteLine=`egrep -v '^[[:space:]]*$' $file`
+
+    echo $firstNonWhiteLine | grep '{' > /dev/null 2>&1
+    startsWithCurly=$?
+
+    echo $firstNonWhiteLine | grep '<html>' > /dev/null 2>&1
+    hasHTMLtag=$?
+
    
-    if [ "$result" -eq "0" ] ; then
+    if [ "$validJSON" -eq "0" ] ; then
+        # If its valid json, then pretty print it
         jsonPrettyPrint.bash $file
-    else 
+
+    elif [ "$startsWithCurly" -eq "0" ] ; then
+        # If it looks like JSON but non valid, show the validator output 
+        echo
+        validateJSON.bash $file
+        echo
+
+    elif [ "$hasHTMLtag" -eq "0" ] ; then
+        # If it looks like HTML, send it to links
         cat $file | lynx --stdin
+
+    else 
+        less $file
+        
     fi
 done
